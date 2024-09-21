b85cf1feaeabbbfea41d3440d3219700d0e54280
Akshet Pandey <argetlam.akshet@gmail.com>
2024-09-21 16:42:41 -0400

    more fixes

--- allauth/socialaccount/providers/klaviyo_oauth2/provider.py
+++ allauth/socialaccount/providers/klaviyo_oauth2/provider.py
@@ -1,3 +1,4 @@
+from allauth.socialaccount import providers
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.klaviyo_oauth2.views import KlaviyoOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
@@ -14,10 +15,11 @@ class KlaviyoProvider(OAuth2Provider):
     oauth2_adapter_class = KlaviyoOAuth2Adapter
 
     def extract_uid(self, data):
-        return data.get('data', {})[0].get('id')
+        return data.get("data", {})[0].get("id")
 
     def get_default_scope(self):
         return ["accounts:read"]
 
 
 provider_classes = [KlaviyoProvider]
+providers.registry.register(KlaviyoProvider)
--- allauth/socialaccount/providers/klaviyo_oauth2/views.py
+++ allauth/socialaccount/providers/klaviyo_oauth2/views.py
@@ -5,41 +5,32 @@ from allauth.socialaccount.providers.oauth2.views import (
     OAuth2CallbackView,
     OAuth2LoginView,
 )
-from django.conf import settings
-
-from .provider import KlaviyoProvider
 
 
-ACCESS_TOKEN_URL = (
-    getattr(settings, "SOCIALACCOUNT_PROVIDERS", {})
-    .get("", {})
-    .get("ACCESS_TOKEN_URL", "https://a.klaviyo.com/oauth/token")
-)
-
-AUTHORIZE_URL = (
-    getattr(settings, "SOCIALACCOUNT_PROVIDERS", {})
-    .get("", {})
-    .get("AUTHORIZE_URL", "https://www.klaviyo.com/oauth/authorize")
-)
-
-KLAVIYO_API_VERSION = '2023-10-15'
+KLAVIYO_API_VERSION = "2023-10-15"
 
 
 class KlaviyoOAuth2Adapter(OAuth2Adapter):
-    provider_id = KlaviyoProvider.id
-    access_token_url = ACCESS_TOKEN_URL
-    authorize_url = AUTHORIZE_URL
+    provider_id = "klaviyo_oauth2"
     basic_auth = True
+
+    @property
+    def access_token_url(self):
+        return "https://a.klaviyo.com/oauth/token"
+
+    @property
+    def authorize_url(self):
+        return "https://www.klaviyo.com/oauth/authorize"
 
     def complete_login(self, request, app, access_token, **kwargs):
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
             "revision": KLAVIYO_API_VERSION,
-            "Authorization": f'Bearer {access_token.token}',
+            "Authorization": f"Bearer {access_token.token}",
         }
         r = requests.get(
-            'https://a.klaviyo.com/api/accounts/',
+            "https://a.klaviyo.com/api/accounts/",
             headers=headers,
         )
         extra_data = r.json()
