1f7b1513c650c756e1be40adbaace42288916c49
CharlesBergmeier <charles.bergmeier@gmail.com>
2025-03-06 17:34:42 -0500

    fix2

--- allauth/socialaccount/providers/klaviyo_oauth2/provider.py
+++ allauth/socialaccount/providers/klaviyo_oauth2/provider.py
@@ -15,7 +15,10 @@ class KlaviyoProvider(OAuth2Provider):
     oauth2_adapter_class = KlaviyoOAuth2Adapter
 
     def extract_uid(self, data):
-        return data.get("data", [{}])[0].get("id")
+        data = data.get("data", [{}])
+        if data:
+            return data[0].get("id")
+        return None
 
     def get_default_scope(self):
         return ["accounts:read"]
